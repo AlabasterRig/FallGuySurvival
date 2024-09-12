e8a5a53ba0d6997edacdbc1282cb213313051fcb
AlabasterRig <utkrist.j@gmail.com>
2024-09-13 03:22:23 +0530

    Few lines of repetitive code deleted
    UpdateInteractionText_Implementation() code to be exact.

--- Source/FallGuySurvival/Private/BaseClass/TFPlayerCharacter.cpp
+++ Source/FallGuySurvival/Private/BaseClass/TFPlayerCharacter.cpp
@@ -28,15 +28,13 @@ void ATFPlayerCharacter::TraceForInteraction()
 	FVector LTEnd = (FollowCamera->GetForwardVector() * SearchLength) + LTStart;
 
 	GetWorld()->LineTraceSingleByChannel(LTHit, LTStart, LTEnd, ECC_Visibility, LTParams);
-
+	UpdateInteractionText_Implementation();
 	if (!LTHit.bBlockingHit || !LTHit.GetActor()->Implements<UInteractionInterface>())
 	{
 		InteractionActor = nullptr;
-		UpdateInteractionText_Implementation();
 		return;
 	}
 	InteractionActor = LTHit.GetActor();
-	UpdateInteractionText_Implementation();
 }
 
 void ATFPlayerCharacter::Move(const FInputActionValue& Value)
@@ -195,10 +193,6 @@ void ATFPlayerCharacter::OnInteractionTriggerOverlapEnd(UPrimitiveComponent* Ove
 	}
 	InteractableActors.Remove(OtherActor);
 	bEnableRayTrace = InteractableActors.Num() > 0;
-	if (!bEnableRayTrace)
-	{
-		UpdateInteractionText_Implementation();
-	}
 }
 
 void ATFPlayerCharacter::UpdateInteractionText_Implementation()
